fd722409e6cd724726b30001d8113d2d892de679
Hemamala-MN <84277583+Hemamala-MN@users.noreply.github.com>
2021-06-27 15:29:44 +0530

    Program to calculate sum of two numbers

---
+++ sumofnum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+
+    int a,b,sum;
+    printf("enter two integer numbers\n");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("the sum of two integers is %d",sum);
+
+}
+
+
